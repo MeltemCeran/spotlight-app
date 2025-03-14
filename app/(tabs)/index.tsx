79811d8d2ffe7614a2591fc14f33e76c8b8e7935
Meltem Ceran <mltm.crn@gmail.com>
2025-03-14 17:31:52 +0300

    Convex ile database bağlantısı oluşturuldu projeye Svix entegre edildi.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,27 +1,37 @@
-import { View } from "react-native";
+import { TouchableOpacity, View, Text } from "react-native";
 import { styles } from "../../styles/auth.styles";
 import { Link } from "expo-router";
+import { useAuth } from "@clerk/clerk-react";
 
 export default function Index() {
+  const { signOut } = useAuth();
   return (
     <View style={styles.container}>
-      <Link href={"/notifications"}>Feed screen in tabs</Link>
-      {/* <Text style={styles.title}>Hello</Text> */}
-      {/* <TouchableOpacity onPress={() => alert("you touched")}>
+      <TouchableOpacity onPress={() => signOut()}>
+        <Text style={{ color: "white" }}>Signout</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+/* <Text style={styles.title}>Hello</Text> */
+
+/* <TouchableOpacity onPress={() => alert("you touched")}>
         <Text>Press Me</Text>
       </TouchableOpacity>
 
       <Pressable onPress={() => alert("you touched")}>
         <Text>Press Me-pressable</Text>
-      </Pressable> */}
-      {/* touchable ve pressable arasındaki fark touchablede tıklandığında text üzerinde renk değişimi oluyor. */}
+      </Pressable> */
 
-      {/* <Image
+/* touchable ve pressable arasındaki fark touchablede tıklandığında text üzerinde renk değişimi oluyor. */
+
+/* <Image
         source={require("../assets/images/icon.png")}
         style={{ width: 100, height: 100 }}
-      /> */}
+      /> */
 
-      {/* <Image
+/* <Image
         source={{
           uri: "https://images.unsplash.com/photo-1741070487520-907d1359cb95?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         }}
@@ -29,8 +39,6 @@ export default function Index() {
           width: 200,
           height: 200,
           resizeMode: "cover",
-        }} */}
-      {/* /> */}
-    </View>
-  );
-}
+        }} */
+
+/* /> */
